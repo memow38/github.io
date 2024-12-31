51fdd04b0a36e338085832853fbb64109866194c
memow38 <memow_@hotmail.com>
2024-12-31 13:07:54 +0100

    Delete project directory

--- project/src/components/About.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function About() {
-  return (
-    <section id="over-ons" className="py-20 bg-gray-50">
-      {/* Existing content */}
-    </section>
-  );
-}
--- project/src/components/Certificate.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function Certificate() {
-  return (
-    <section id="certificaat" className="py-20 bg-white">
-      {/* Existing content */}
-    </section>
-  );
-}
